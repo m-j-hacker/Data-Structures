eb1f2b6e4685704bc4090affb948b7e8cc325f1e
m-j-hacker <mjhacker@gmail.com>
2019-05-15 12:23:50 -0500

    Complete: insert definition

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,23 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    # first, we need to determine if the value is less or greater than self.value
+
+    # If value is less than and there is not already a value on the left, assign value to left
+    if value < self.value:
+
+      if not self.left:
+        self.left = BinarySearchTree(value)
+    # If there is a value on the left already, run insert recursively
+      else:
+        self.left.insert(value)
+
+    # If value is greater than or equal to self.value, assign value to right
+    elif value >= self.right:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
     pass
